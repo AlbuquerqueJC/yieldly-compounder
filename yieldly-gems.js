b5fcf71933ccf5d54af9f55a052753bbe2a76436
John A <john.albuquerque@gmail.com>
2021-12-02 15:47:04 -0500

    algo/yldy reversed under claim rewards

--- yieldly-gems.js
+++ yieldly-gems.js
@@ -336,7 +336,7 @@ const log = message => {
 
             // id=233725850 YLDY-YLDY/ALGO
             const claimedPoolRewards = await claimPoolRewards(233725850);
-            log(`Claimed YLDY Pool Assets: ${claimedPoolRewards[0]} YLDY | ${claimedPoolRewards[1]} ALGO`)
+            log(`Claimed YLDY Pool Assets: ${claimedPoolRewards[0]} ALGO | ${claimedPoolRewards[1]} YLDY`)
 
             // *******************************
             // STAKE - EVERY YLDY FROM WALLET
